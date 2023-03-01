65947db68771d50f754d6fd62f2042f0bc1e06e6
ClaudioF9 <claudioferreira0284@gmail.com>
2023-03-01 02:21:16 +0000

    finally was able to debug the license badge issue

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // console.log("hello");
 const fs = require("fs");
-const path = require('path');
+// const path = require('path');
 const inquirer = require('inquirer');
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // // array of questions for user
 const promptUser = () => 
@@ -10,11 +11,25 @@ const promptUser = () =>
       type: 'input',
       name: 'title',
       message: 'What is the title of your project?',
+      validate: function (data) {
+        if (data.length < 1) {
+          return console.log("Please enter a title for your project");
+        } else {
+          return true;
+        }
+      }
     },
     {
       type: 'input',
       name: 'description',
       message: 'Please add a description to your project',
+      validate: function (data) {
+        if (data.length < 1) {
+          return console.log("Please enter a valid description for your project");
+        } else {
+          return true;
+        }
+      }
     },
     {
       type: 'input',
@@ -30,12 +45,12 @@ const promptUser = () =>
       type: 'checkbox',
       message: 'Which license have you used?',
       name: 'license',
-      choices: ["None", "MIT", "GPLv2", "Apache" ],
+      choices: ["GNU GPL v3", "Apache 2.0", "MIT", "The Unlicensed"],
     },
     {
       type: 'input',
       name: 'credits',
-      message: 'Please add list of contributors and collaborators',
+      message: 'Please add list of contributors and collaborators if any',
     },
     {
       type: 'input',
@@ -46,79 +61,35 @@ const promptUser = () =>
       type: 'input',
       name: 'username',
       message: 'What is your Github username?',
+      validate: function (data) {
+        if (data.length < 1) {
+          return console.log("Please enter a valid Github username");
+        } else {
+          return true;
+        }
+      }
     },
     {
       type: 'input',
       name: 'email',
       message: 'What is your email address?',
+      validate: function (data) {
+        if (!data.includes("@")) {
+          return console.log("Please enter a valid email address");
+        } else {
+          return true;
+        }
+      }
     }
 ]);
 
-const generateMarkdown = (data) => {
-return `# ${data.title}      
-
-## Description
-
-${data.description}
-
-## Table of Contents
-
-Installation
-Usage
-License
-Contributing
-Tests
-Questions
-
-## Installation
-
-${data.installation}
-
-## Usage
-
-${data.usage}
-
-## License
-
-${data.license}
-
-## Credits
-
-${data.credits}
-
-## Tests
-
-${data.test}
-
-## Questions
-
-My GitHub profile is github.com/${data.username} should you want to get into contact or view my other projects.
-
-Feel free to email me at ${data.email} if you have any questions regarding the project.
-
-`;}
-
+//--------------------------------------------------------------------------------------------------
 promptUser()
   .then((data) => fs.writeFile('readme.md', generateMarkdown(data), (err) => {
     if (err) throw err;
   }))
   .then(() => console.log('Successfully created your readme.md!!'))
-  // .catch((err) => console.error(err));
+  //------------------------------------------------------------------------------------------------
 
 
-
-
-
-
-
-// function to write README file
-// function writeToFile(fileName, data) {
-// }
-
-// function to initialize program
-// function init() {
-
-// }
-
-// function call to initialize program
-// init();
+  
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,47 +1,85 @@
 // function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}      
+function generateBadge(data) {
+  const licenseType = data.license[0];
+  let badge = ""
+  if (licenseType === "Apache 2.0") {
+      badge = "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+  } else if (licenseType === "GNU GPL v3") {
+      badge = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)"
+  } else if (licenseType === "MIT") {
+      badge = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+  } else if (licenseType === "The Unlicensed") {
+      badge = "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)"
+  } else {
+      badge = ""
+  };
+  return badge; // 
+};
+
+
+//---------------------------------------------------------------------------------------------
+const generateMarkdown = (data) => {
+return `# ${data.title}      
+
+${generateBadge(data)}
+
 
 ## Description
+---
 
 ${data.description}
 
 ## Table of Contents
 
-Installation
-Usage
-License
-Contributing
-Tests
-Questions
+1. [Installation](#installation)
+2. [Usage](#usage)
+3. [License](#license)
+4. [Contributing](#contributing)
+5. [Tests](#tests)
+5. [Questions](#questions)
 
-## Installation
+## Installation 
+---
+<a name="#installation"></a>
+
 
 ${data.installation}
 
-## Usage
+## Usage 
+---
+<a name="#usage"></a>
 
 ${data.usage}
 
-## License
+## License 
+---
+<a name="#license"></a>
 
-${data.license}
+This project is licensed using the ${data.license} license.
 
-## Credits
+## Credits 
+---
+<a name="#contributing"></a>
 
 ${data.credits}
 
-## Tests
+## Tests 
+---
+<a name="#tests"></a>
 
-${data.test}
+${data.tests}
 
-## Questions
+## Questions 
+---
+<a name="#questions"></a>
 
 My GitHub profile is github.com/${data.username} should you want to get into contact or view my other projects.
 
 Feel free to email me at ${data.email} if you have any questions regarding the project.
 
-`;
-}
+`;}
+//---------------------------------------------------------------------------------------------
+
+
 
 module.exports = generateMarkdown;
